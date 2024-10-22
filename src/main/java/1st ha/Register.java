31a8eb60827eddd3c3de570d6790595e23f53f97
DoHa2512 <23020529@vnu.edu.vn>
2024-10-22 11:52:52 +0700

    cuoi chong ko?

---
+++ src/main/java/1st ha/Register.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Register {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button log;
+
+    @FXML
+    void initialize() {
+        assert log != null : "fx:id=\"log\" was not injected: check your FXML file 'Register.fxml'.";
+
+    }
+
+}
